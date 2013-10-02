d6ed788e7e9e5e042b58f20bff2493c0e69f8e7f
Matthew Struble <Matt@MayateckInnovations.net>
2013-10-02 15:14:03 -0700

    Created ShopItem class.

--- src/net/mayateck/VillagerVendor/ItemBookHandler.java
+++ src/net/mayateck/VillagerVendor/ItemBookHandler.java
@@ -1,11 +1,23 @@
 package net.mayateck.VillagerVendor;
 
+import java.util.Arrays;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemBookHandler {
 	public static void givePlayerItemBook(Player p){
-		ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
+		ItemStack book = new ItemStack(Material.EMERALD, 1);
+		ItemMeta data = book.getItemMeta();
+		data.setDisplayName("§6VillagerVendor §7Shop Item");
+		data.setLore(Arrays.asList("ItemID: 0", "Damage Value: 0", "Amount: 0", "Cost: 0"));
+		// TODO Enchantments and NBT Tags
+		if (p.getItemInHand()==new ItemStack(Material.AIR)){
+			p.setItemInHand(book);
+		} else {
+			p.getInventory().addItem(book);
+		}
 	}
 }
